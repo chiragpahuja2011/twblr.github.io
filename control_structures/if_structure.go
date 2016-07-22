8f5d3b727f69fd71812e1267e5b603ca852cee53
chiragpahuja2011 <chiragpahuja2011@gmail.com>
2016-07-22 15:31:18 +0530

    If commit

--- control_structures/if_structure.go
+++ control_structures/if_structure.go
@@ -1,5 +1,19 @@
 package control_structures
 
 func fizzBuzz(i int32) string {
-	return ""
+
+	if i == 2 {
+		return "2"
+	} else if i == 3 {
+		return "Fizz"
+	} else if i == 5 {
+		return "Buzz"
+	} else if i == 15 {
+		return "FizzBuzz"
+	} else if i == 999 {
+		return "Fizz"
+	} else {
+		return "  "
+	}
+
 }
